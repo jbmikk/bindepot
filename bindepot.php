3f38d1220d44c4458db29b2bc6ffe5a898548daf
Jens Mikkelsen <jbmikk@gmail.com>
2011-12-10 17:41:50 -0300

    Initial commit

---
+++ bindepot.php
@@ -0,0 +1,82 @@
+<?php
+class FileResult {
+	function __construct($path) {
+		$this->path = $path;
+	}
+}
+
+class BinaryHandler {
+	function store($path, $id, $file) {
+		if(is_array($file)) {
+			move_uploaded_file($file['tmp_name'], "$path/$id");
+		}
+		else {
+			$fh = fopen("$path/$id", 'wb') or die("Cannot open file: $id");
+			fwrite($fh, $file);
+			fclose($fh);
+		}
+	}
+
+	function retrieve($path, $id) {
+		return new FileResult("$path/$id");
+	}
+}
+
+class ImageHandler extends BinaryHandler {
+	function store($path, $id, $file) {
+		parent:store($path, $id, $file);
+	}
+
+	function retrieve($path, $id, $mode) {
+		return parent::retrieve($path, $id, $mode);
+	}
+}
+
+class BinDepot{
+
+	function __construct($store, $conf = 'bindepot.xml') {
+		$this->load_configuration($conf, $store);
+		if(@!file_exists($this->default_path)) {
+			throw new Exception("FileStorage: default storage directory does not exist: $this->default_path");
+		}
+		if(@!is_writable($path)) {
+			throw new Exception("FileStorage: default storage directory is not writable: $this->default_path");
+		}
+		$this->path = "$this->default_path/$store";
+		$this->handler = $this->getHandler($store);
+	}
+
+	private function load_configuration($conf, $store) {
+		$conf = simplexml_load_file($conf);
+
+		$path = $conf->xpath('/file-storage/@default-path');
+		$this->default_path = isset($path[0]) ? $path[0]: '';
+
+		$type = $conf->xpath("/file-storage/store[@name='a$store']/@type");
+		$this->type = isset($type[0]) ? $type[0]: 'binary';
+	}
+
+	private function getHandler($store) {
+		switch($this->type) {
+			case 'image':
+				$handler = new ImageHandler();
+				break;
+			case 'binary':
+			default:
+				$handler = new BinaryHandler();
+				break;
+		}
+		return $handler;
+	}
+
+	function store($id, $file) {
+		if(@!file_exists($this->path))
+			mkdir($this->path, 0777);
+		$handler->store($this->path, $id, $file);
+	}
+
+	function retrieve($id, $format) {
+		return $handler->retrieve($this->path, $id, '');
+	}
+}
+?>
